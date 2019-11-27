e73e0b25a467cc1c0e8a6469f926262083fdce64
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-11-27 22:33:23 +0100

    Created the general setup for the class based discord bot design.

--- ritmo.py
+++ ritmo.py
@@ -1,1 +1,39 @@
-print("Hello world!")
+"""A simple discord bot named Ritmo. Ritmo is a music bot that has expanded functionality through spotify integration.
+
+The discord bot is implemented using a class based design with the "discord.Client" superclass from the
+"discord" python package.
+"""
+import json
+import discord
+
+
+class Ritmo(discord.Client):
+    """
+    Class representing a discord bot object. The function "on_message" from the super class
+    "discord.Clint" is overwritten to implement the functionality of the available commands.
+    """
+
+    async def on_ready(self):
+        """Displaying information about the bot when it is ready to run."""
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        """
+        This method is called every time a message is sent and if the message contains
+        a command then that command is executed via another class method.
+        """
+
+        # Ignore if the message is from the bot itself.
+        if message.author == self.user:
+            return
+
+
+client = Ritmo()
+
+# Pulling the token from the config file and using it to set up the bot.
+with open("config.json", "r") as config:
+    config_dict = json.load(config)
+    client.run(config_dict["token"])
