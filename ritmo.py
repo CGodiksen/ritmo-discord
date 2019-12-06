ce37bccbe98179a80532ee3444c9cbeb2c17a5b3
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2019-12-06 22:58:04 +0100

    Added initial functionality for the !play command.
    This includes a function that can find the first video url in a
    youtube search query.

--- ritmo.py
+++ ritmo.py
@@ -5,6 +5,9 @@ The discord bot is implemented using a class based design with the "discord.Clie
 """
 import json
 import discord
+import song_queue
+from bs4 import BeautifulSoup
+import urllib.request
 
 
 class Ritmo(discord.Client):
@@ -12,6 +15,10 @@ class Ritmo(discord.Client):
     Class representing a discord bot object. The function "on_message" from the super class
     "discord.Clint" is overwritten to implement the functionality of the available commands.
     """
+
+    def __init__(self, **options):
+        self.queue = song_queue.SongQueue()
+        super().__init__(**options)
 
     async def on_ready(self):
         """Displaying information about the bot when it is ready to run."""
@@ -29,6 +36,31 @@ class Ritmo(discord.Client):
         # Ignore if the message is from the bot itself.
         if message.author == self.user:
             return
+
+        if message.content.starswith("!play ", 0, 6):
+            self.queue.push_song(message.content[6:])
+
+    async def play(self, song):
+        pass
+
+    def find_youtube_url(self, song):
+        """
+        Searches youtube using the song as the query and returns the first video url found.
+
+        :param song: The song that we wish to find on youtube.
+        :return: The url of the first video on the page when searching for the specified search query.
+        """
+        text_to_search = song
+
+        query = urllib.parse.quote(text_to_search)
+        url = "https://www.youtube.com/results?search_query=" + query
+
+        response = urllib.request.urlopen(url)
+        html = response.read()
+
+        soup = BeautifulSoup(html, 'html.parser')
+
+        return soup.find_next(attrs={'class': 'yt-uix-tile-link'})
 
 
 client = Ritmo()
