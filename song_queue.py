a584b807696eb4ea8b6c7ed1fb08b3dd752302de
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-04-28 20:34:24 +0200

    Updated song queue so it now supports downloading two songs ahead to increase responsiveness by avoiding the scenario where we download the song when we have to play it.

--- song_queue.py
+++ song_queue.py
@@ -5,16 +5,45 @@ class SongQueue:
     """Class representing a queue containing songs."""
 
     def __init__(self):
+        # Contains songs, each represented by a tuple consisting of the song title and song url.
         self.queue = []
 
-    def push_song(self, search_query):
-        song = youtube.get_youtube_video(search_query, "audio_files/")
-        self.queue.append(song)
+        # We maintain a downloaded queue to keep track of the songs in the queue which are downloaded.
+        # The purpose of this is to download songs before they are to be played to increase responsiveness.
+        self.downloaded_queue = []
 
-        print(self.queue)
+    def push_song(self, title_url):
+        """
+        Adding a song to the queue. Also downloading the song if needed.
+
+        :param title_url: A tuple consisting of a song title and the youtube url to the song.
+        :return: None
+        """
+        # Adding the song to the normal queue.
+        self.queue.append(title_url)
+
+        # If there are still two or less songs in the queue we download it and add the filename to the downloaded songs.
+        if len(self.queue) <= 2:
+            song_file = youtube.download_mp3(title_url[1], "audio_files/")
+            self.downloaded_queue.append(song_file)
 
     def pop_song(self):
-        return self.queue.pop(0)
+        """
+        Removing the first song in the queue and popping the song filename from the downloaded queue.
+
+        :return: The filename of the song that is first in the queue.
+        """
+        del self.queue[0]
+        return self.downloaded_queue.pop(0)
+
+    def update_downloaded_queue(self):
+        """
+        Updating the queue containing downloaded songs if there are any songs in the normal queue that are not
+        downloaded yet.
+        """
+        if len(self.queue) >= 2:
+            song_file = youtube.download_mp3(self.queue[1][1], "audio_files/")
+            self.downloaded_queue.append(song_file)
 
     def __str__(self):
         """String representation of the entire song queue."""
@@ -24,7 +53,7 @@ class SongQueue:
         # If there are any songs in the queue we list the song names in a numbered list.
         if self.queue:
             for counter, song in enumerate(self.queue):
-                queue_str += str(counter + 1) + ". " + song[24:-4] + "\n"
+                queue_str += str(counter + 1) + ". " + song[0] + "\n"
         else:
             queue_str += "The queue is empty."
 
